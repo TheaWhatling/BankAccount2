31e7b348fb7eaaf0cb72a410c020ed385933c326
Thea Whatling <K038218@eltham-college.org.uk>
2020-11-07 14:55:36 +0000

    Add project files.

---
+++ BankAccount2/BankAccount.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount2
+{
+    class BankAccount
+    {
+    }
+}
---
+++ BankAccount2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BankAccount2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a bank account class, attributes should include name, balance, address and bankaccount number.
+
+            //Create get set methods, and withdraw, deposit and print statement to screen message.
+        }
+    }
+}
