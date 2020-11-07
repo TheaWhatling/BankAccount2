2e742359e6b1f56f8ec7a21afcff85f9c7d947ca
Thea Whatling <K038218@eltham-college.org.uk>
2020-11-07 15:47:29 +0000

    created class, its attributes and get set methods

--- BankAccount2/BankAccount.cs
+++ BankAccount2/BankAccount.cs
@@ -6,5 +6,102 @@ namespace BankAccount2
 {
     class BankAccount
     {
+
+        private string name;
+        private int balance;
+        private string address;
+        private int bankaccountnum;
+
+        public BankAccount()
+        {
+            name = "test";
+            balance = 0;
+            address = "London";
+            bankaccountnum = 0;
+        }
+
+        public BankAccount(string name)
+        {
+            this.name = name;
+            this.balance = 0;
+            this.address = "London";
+            this.bankaccountnum = 0;
+        }
+
+        public BankAccount(string name, int balance)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.address = "London";
+            this.bankaccountnum = 0;
+        }
+
+        public BankAccount(string name, int balance, string address)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.address = address;
+            this.bankaccountnum = 0;
+        }
+
+        public BankAccount(string name, int balance, string address, int bankaccountnum)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.address = address;
+            this.bankaccountnum = bankaccountnum;
+        }
+
+        public void setname(string name) 
+        {
+            this.name = name;
+        }
+
+        public string getname()
+        {
+            return name;
+        }
+
+        public void setbalance(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public int getbalance()
+        {
+            return balance;
+        }
+
+        public void setaddress(string address)
+        {
+            this.address = address;
+        }
+
+        public string getaddress()
+        {
+            return address;
+        }
+
+        public void setbankaccountnum(int bankaccountnum)
+        {
+            this.bankaccountnum = bankaccountnum;
+        }
+
+        public int getbankaccountnum()
+        {
+            return bankaccountnum;
+        }
+
+        //public void print(string name, int balance, string address, int bankaccountnum)
+        //{
+        //    Console.WriteLine("name: " + name);
+        //    Console.WriteLine("balance: " + balance);
+        //    Console.WriteLine("address: " + address);
+        //    Console.WriteLine("bank account number: " + bankaccountnum);
+        //}
+
+
+
+
     }
 }
--- BankAccount2/Program.cs
+++ BankAccount2/Program.cs
@@ -6,9 +6,56 @@ namespace BankAccount2
     {
         static void Main(string[] args)
         {
+            int t = 0;
             //Create a bank account class, attributes should include name, balance, address and bankaccount number.
 
             //Create get set methods, and withdraw, deposit and print statement to screen message.
+
+            BankAccount[] s = new BankAccount[10];
+
+            for (int x = 0; x < 10; x = x + 1)
+            {
+                s[x] = new BankAccount();
+
+                string enteredname = " ";
+                Console.WriteLine("please enter name");
+                enteredname = Console.ReadLine();
+                s[x].setname(enteredname);
+
+                int enteredbalance = 0;
+                Console.WriteLine("please enter balance");
+                enteredbalance = Convert.ToInt32(Console.ReadLine());
+                s[x].setbalance(enteredbalance);
+
+                string enteredaddress = " ";
+                Console.WriteLine("please enter your address/city");
+                enteredaddress = Console.ReadLine();
+                s[x].setaddress(enteredaddress);
+
+                int enteredbankaccountnum = 0;
+                Console.WriteLine("please enter your bankaccount number");
+                enteredbankaccountnum = Convert.ToInt32(Console.ReadLine());
+                s[x].setbankaccountnum(enteredbankaccountnum);
+
+                t = x;
+
+            }
+
+            Console.WriteLine("information: ");
+            Console.WriteLine("name: " + s[t]getname);
+            Console.WriteLine("balance: " + s[t]getbalance);
+            Console.WriteLine("address: " + s[t]getaddress);
+            Console.WriteLine("bank account number: " + s[t]getbankaccountnum);
+
+            //Console.WriteLine(s[t].print());
+
+            //int hoursinput = 0;
+            //Console.WriteLine("enter the number of hours " + s[x].getName() + " has worked"); // change so v can change 
+            //hoursinput = Convert.ToInt32(Console.ReadLine());
+            //s[x].learn(hoursinput);
+
+
         }
     }
 }
+ 
